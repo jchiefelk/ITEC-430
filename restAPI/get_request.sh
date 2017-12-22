da12dd4469d08e9d88abf43585b8889628fe8b39
Jackson Chief Elk Admin <facjcadmin@ITE13576.local>
2017-12-22 11:06:06 -0700

    Post and Get Requests to RestAPI. flask-mysql middleware added to connect to database 'testuserdb'

--- restAPI/get_request.sh
+++ restAPI/get_request.sh
@@ -1,1 +1,1 @@
-curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X GET http://localhost:5000/hello?password=111
+curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X GET http://localhost:5000/api?password=111
--- restAPI/rest.py
+++ restAPI/rest.py
@@ -1,17 +1,43 @@
 from flask import Flask, request
+from flask.ext.mysql import MySQL
 app = Flask(__name__)
+
+# mySQL Configurations
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER']='root'
+app.config['MYSQL_DATABASE_PASSWORD']=''
+app.config['MYSQL_DATABASE_DB']='testuserdb'
+app.config['MYSQL_DATABASE_HOST']='127.0.0.1'
+mysql.init_app(app)
+conn = mysql.connect()
+
+
 password = {
 	'secret': '111' 	
 };
-@app.route('/hello')
-def api_hello():
-	if 'password' in request.args and password['secret']==request.args['password']:
-		response ={
+
+@app.route('/api',methods = ['GET', 'POST', 'DELETE'])
+def api_actions():
+	if request.method =='GET':
+
+		if 'password' in request.args and password['secret']==request.args['password']:
+			response={
 			'message': 'Password '+request.args['password']+' correct'+'\n'
-		}
-		return response['message']
-	else:
-		return 'Incorrect Password'
+			}
+			return response['message']
+		else:
+			return 'Incorrect Password\n'
+
+
+	elif request.method =='POST':
+
+		if 'password' in request.args and password['secret']==request.args['password']:
+			response={
+			'message': 'Password '+request.args['password']+' correct'+'\n'
+			}
+			return response['message']
+		else:
+			return 'Incorrect Password\n'
 
 if __name__ == "__main__":
 	app.run(debug=True)

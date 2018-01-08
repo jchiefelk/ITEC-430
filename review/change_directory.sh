0b807f4ba5bbbe1e2a2e2a8854a49850a8cf6c4f
Jackson Chief Elk Admin <facjcadmin@ITE13576.local>
2018-01-08 16:23:14 -0700

    Introduction

---
+++ review/change_directory.sh
@@ -0,0 +1,1 @@
+cd directory_name
---
+++ review/using_change_directory.sh
@@ -0,0 +1,6 @@
+# Changes to the root directory because its the absolute pathname
+# cd /
+# Changes to current directory
+# cd .
+# Changes to the Parent directory
+# cd ..

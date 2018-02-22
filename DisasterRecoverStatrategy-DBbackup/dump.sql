79854a58e8d1dea086260958e361a68a6aeb2428
Jackson Chief Elk Admin <facjcadmin@ITE13576.local>
2018-02-22 08:39:35 -0700

    homework 4 folder

--- DisasterRecoverStatrategy-DBbackup/dump.sql
+++ DisasterRecoverStatrategy-DBbackup/dump.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: employees
 -- ------------------------------------------------------
--- Server version	5.7.20
+-- Server version	5.7.21
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -102,4 +102,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-02-06 20:37:10
+-- Dump completed on 2018-02-19 17:53:59
